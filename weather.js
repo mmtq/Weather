375ba94d3f25648787cfe51c8c31c574994b7599
Sayemahamed <sayemahamed183@gmail.com>
2024-03-20 23:32:00 +0600

    don't know what , done something

--- weather.js
+++ weather.js
@@ -1,53 +1,37 @@
-const express = require('express');
-const https = require('https');
-const bodyParser = require('body-parser');
+const express = require("express");
+const https = require("node:https");
+const bodyParser = require("body-parser");
 // const geoip = require('geoip-lite');
-const path = require('path');
+const path = require("path");
 
 const app = express();
-app.use(express.static(path.join(__dirname, '/')));
-
-var userCity = "";
+app.use(express.static(path.join(__dirname, "/")));
 
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use((req, res, next) => {
-//     const ip = req.headers['x-forwarded-for'] || req.remoteAddress;
-//     req.clientIp = ip;
-//     next();
-// });
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
-
-    // const ip = req.clientIp;
-    // const geo = geoip.lookup(ip);
-    // const city = geo ? geo.city : 'Chitagong';
-    // userCity=(`Your city: ${city}`);
-});
-let weatherData;
-app.post('/data', function (req, res) {
-    const query = req.body.cityName;
-    const apiKey = "739e0bae73dd35447089356bf29cabd8";
-    const unit = "metric";
-    const url = "https://api.openweathermap.org/data/2.5/weather?appid=" + apiKey + "&q=" + query + "&units=" + unit;
-
-    // https.get(url, function (response) {
-    //     response.on('data', function (data) {
-
-    //         res.send(data);
-    //     });
-    // });
-    var data= {
-        name: "weather"
-    }
-    
-    res.send(JSON.stringify(data));
-});
-app.get('/data', (req, res) => {
-    res.json({ data: weatherData })
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
+app.post("/data", function (req, res) {
+  const query = req.body.cityName;
+  console.log(query);
+  const apiKey = "739e0bae73dd35447089356bf29cabd8";
+  const unit = "metric";
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?appid=" +
+    apiKey +
+    "&q=" +
+    query +
+    "&units=" +
+    unit;
+  https.get(url, function (response) {
+    response.on("data", function (data) {
+      res.send(data);
+    });
+  });
+});
 
 app.listen(3000, function () {
-    console.log('listening on port 3000...');
+  console.log("listening on port 3000...");
 });
